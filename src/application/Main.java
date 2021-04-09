22a45f9adb2a74c67ab474821045f8d872168ffb
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-04-08 22:56:07 -0400

    Test to show StackTrace.

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,23 +7,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		method1();
+		System.out.println("End of program!");
+		
+	}
+	
+	public static void method1() {
+		System.out.println("=== METHOD 1 START ===");
+		method2();
+		System.out.println("=== METHOD 1 END ===");
+	}
+	
+	public static void method2() {
+		System.out.println("=== METHOD 2 START ===");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
-			System.out.print(vect[position]);
+			System.out.println(vect[position]);
 		}
 		catch(ArrayIndexOutOfBoundsException e){
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
 		}
 		catch(InputMismatchException e) {
 			System.out.println("Input error!");
 		}
 		
-		System.out.println("End of program!");
-		
 		sc.close();
+		System.out.println("=== METHOD 2 END ===");
 	}
 
 }
